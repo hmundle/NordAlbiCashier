f059fe5d85427562c57960010c4466d18d280bd7
Heiko Mundle <heiko.mundle@gaf.de>
2023-07-05 18:36:39 +0200

    Show details of the invoice using JOIN statements

--- Nac.Dal/Repos/InvoiceRepo.cs
+++ Nac.Dal/Repos/InvoiceRepo.cs
@@ -14,4 +14,21 @@ public class InvoiceRepo : BaseEntityRepo<Invoice>, IInvoiceRepo
     : base(options)
     {
     }
+
+    public override Task<Invoice?> FindAsNoTrackingAsync(Guid id)
+    {
+        throw new NotImplementedException();
+        //return base.FindAsNoTrackingAsync(id);
+    }
+
+    public override IQueryable<Invoice> FindAsQuery(Guid? id)
+    {
+        return base.FindAsQuery(id).Include(i => i.Sellings).ThenInclude(s => s.ProductNavigation);
+    }
+
+    public override async Task<Invoice?> FindAsync(Guid? id)
+    {
+        var invoice = await FindAsQuery(id).FirstOrDefaultAsync();
+        return invoice;
+    }
 }
