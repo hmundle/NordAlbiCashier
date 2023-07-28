534a82dcfc5e7f33535426dcf9cc1d179666b7ac
Heiko Mundle <heiko.mundle@gaf.de>
2023-07-28 23:55:16 +0200

    Add scripts to initialize the DB

---
+++ initialUsers.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.users (id,"name",created,modified,is_sychronized,is_deleted,"operator") VALUES
+	 ('d6a0aa3e-b20c-4892-9d62-6243af61cf89'::uuid,'Heiko','2023-06-26 08:25:47.559847+02','2023-06-26 08:25:47.559847+02','local'::sync_status::sync_status,false,'unknown'),
+	 ('bae61716-bfdd-4842-b7b9-90ba2b18befd'::uuid,'Kasse1','2023-07-27 09:04:49.156977+02',NULL,'local'::sync_status::sync_status,false,'unknown'),
+	 ('4d5a59a0-f10a-479c-8a3e-3d4dc678a57d'::uuid,'Kasse2','2023-07-27 09:04:49.1612+02',NULL,'local'::sync_status::sync_status,false,'unknown'),
+	 ('7baf3665-4ca8-48cc-9f1b-b4b51c5902fe'::uuid,'Tom','2023-07-27 09:05:10.668289+02',NULL,'local'::sync_status::sync_status,false,'unknown');
